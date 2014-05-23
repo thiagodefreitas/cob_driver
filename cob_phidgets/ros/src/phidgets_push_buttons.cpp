4530431d85fe855c3ff473b62a87f2539a796c28
thiagodefreitas <thiagodefreitas@gmail.com>
2014-05-23 09:14:45 +0200

    Adds license and comment on the server generation

--- cob_phidgets/ros/src/phidgets_push_buttons.cpp
+++ cob_phidgets/ros/src/phidgets_push_buttons.cpp
@@ -1,3 +1,62 @@
+/*!
+ *****************************************************************
+ * \file
+ *
+ * \note
+ *   Copyright (c) 2014 \n
+ *   Fraunhofer Institute for Manufacturing Engineering
+ *   and Automation (IPA) \n\n
+ *
+ *****************************************************************
+ *
+ * \note
+ *   Project name: care-o-bot
+ * \note
+ *   ROS stack name: cob_driver
+ * \note
+ *   ROS package name: cob_phidgets
+ *
+ * \author
+ *   Author: Thiago de Freitas, mail: tdf@ipa.fhg.de
+ * \author
+ *   Supervised by: Thiago de Freitas, mail: tdf@ipa.fhg.de
+ *
+ * \date Date of creation: 23.05.2014
+ *
+ * \brief
+ *   this package integrates phidget boards into the ros system
+ *
+ *****************************************************************
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     - Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer. \n
+ *     - Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution. \n
+ *     - Neither the name of the Fraunhofer Institute for Manufacturing
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission. \n
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
 #include "ros/ros.h"
 #include "std_msgs/Bool.h"
 #include "sensor_msgs/Range.h"
@@ -147,6 +206,7 @@ int main(int argc, char **argv)
     int numInputs, numOutputs, numbuttons;
 	int err;
 
+    /* This adds the Server to be called to get the actual state of the buttons */
     ros::ServiceServer service = nh_.advertiseService("button_state",update_button_state);
     ros::ServiceClient client = nh_.serviceClient<cob_srvs::SetString>("button_state");
 
